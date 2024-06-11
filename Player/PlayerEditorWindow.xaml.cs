8e983b7829f559f749dfe2574d8f2809bfb5f2b7
Joe Long <joelong@outlook.com>
2024-06-10 18:25:00 -0700

    use of WinUIEx

--- Player/PlayerEditorWindow.xaml.cs
+++ Player/PlayerEditorWindow.xaml.cs
@@ -3,6 +3,7 @@ using Catan3.Models;
 using Microsoft.UI.Windowing;
 using Windows.Graphics;
 using Microsoft.UI;
+using WinUIEx;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 namespace Catan3.Player
@@ -10,32 +11,25 @@ namespace Catan3.Player
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class PlayerEditorWindow : Window
+    public sealed partial class PlayerEditorWindow : WinUIEx.WindowEx
     {
         public static PlayerEditorWindow? EditorWindow;
         public PlayerEditorWindow()
         {
             this.InitializeComponent();
             EditorWindow = this;
-            var appWindowPresenter = this.AppWindow.Presenter as OverlappedPresenter ;
-            if (appWindowPresenter is not null)
-            {
-              //  appWindowPresenter.IsResizable = false;
-            }
 
-            var appWindow = GetAppWindowForCurrentWindow();
 
-            // Set the window size
-            appWindow.Resize(new SizeInt32(975, 1200));
-        //    WindowManager.Get(this).IsMaximizable = false;
+           
+            var windowManger = WindowManager.Get(this);
+            windowManger.IsMaximizable = false;
+            windowManger.IsResizable = false;
+            windowManger.AppWindow.Resize(new SizeInt32(975, 1200));
+            windowManger.PersistenceId = "Catan_Player_Editor";
+            
         }
 
-        private AppWindow GetAppWindowForCurrentWindow()
-        {
-            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            return AppWindow.GetFromWindowId(windowId);
-        }
+
         /// <summary>
         ///     Allow the Page's view model to be set when you have a reference to the Window
         /// </summary>
